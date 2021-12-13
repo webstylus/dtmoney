bc132252a441d1f3d12f7c701ff77d7bbc447684
Rafael Pacífico <rafael@webstylus.com.br>
2021-12-13 20:38:25 -0300

    Add styled components and configure globalstyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <GlobalStyle />
     </div>
   );
 }
